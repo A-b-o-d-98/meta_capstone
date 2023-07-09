a6d60b448b6162e674f40e0b3f06cc84a67ab845
TheKnife <abd9994049@gmail.com>
2023-07-09 19:23:19 +0400

    lcommit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from 'react';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import Nav from './Nav';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    < >
-      <Nav/>
+    <div>
+    <React.Fragment>
+      <Nav />
       <Header/>
-      <Main/>
-      <Footer/>
-
-    </>
+      <Main />
+      <Footer />
+    </React.Fragment>
+    </div>
   );
 }
 
