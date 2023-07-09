0b31d216a3f4256b625a997831c64e029e466f80
TheKnife <abd9994049@gmail.com>
2023-07-09 18:13:31 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-       hi
-    </div>
+    < >
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
+
+    </>
   );
 }
 
